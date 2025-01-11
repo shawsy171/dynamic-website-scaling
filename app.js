e39acbb1360a86af517a6e49dacfcb85e467fec1
Damien Shaw <damien.shaw@hotmail.co.uk>
2025-01-11 17:56:59 +0000

    add health and data get endpoints

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
+// Originally from https://github.com/academind/aws-demos.git
+
 const fs = require('node:fs/promises');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const PORT = 3000;
 
 app.use(bodyParser.json());
 
+
+
 app.get('/', async function (req, res) {
   let dummyData;
-  for (let i = 0; i < 5000; i++) {
+  // for (let i = 0; i < 5000; i++) {
+    dummyData = await fs.readFile('data.json');
+  // }
+  const json = JSON.parse(dummyData);
+  res.json({ message: 'Success!', data: json });
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+app.get('/data/:amount', async function (req, res) {
+  const amount = req.params.amount;
+  console.log({ amount })
+
+  const numAmount = parseInt(amount, 10);
+  for (let i = 0; i < numAmount; i++) {
     dummyData = await fs.readFile('data.json');
   }
+
   const json = JSON.parse(dummyData);
   res.json({ message: 'Success!', data: json });
 });
@@ -22,4 +44,6 @@ app.post('/data', async function (req, res) {
   res.status(201).json({ message: 'Received dummy data.', data });
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}`)
+});
